f26cdc28830d5ff01e6d28998abe779cf8d16e2d
Muhammad Moeez <moeezshkh9777@gmail.com>
2025-01-25 09:41:09 -0800

    Smit logo page backend

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from "dotenv";
+import authRoutes from './routers/auth.js'
+
+
+const app = express()
+const PORT = 4000
+
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json())
+app.use(cors('*'))
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>console.log('MONGODB CONNECTED SUCCESSFULLY')
+)
+.catch((err)=> console.log('ERROR IN CONNECTION',err)
+)
+
+
+
+app.get('/',(req,res)=>{
+    console.log('FETCH  SUCCESSFULLY AND');
+    res.send("FETCH  SUCCESSFULLY AND API CALL SUCCESSFULLY")
+    
+})
+app.get('/check',(req,res)=>{
+    console.log('check  SUCCESSFULLY api');
+    res.send("FETCH  SUCCESSFULLY AND API CALL SUCCESSFULLY about")
+    
+})
+app.use('/auth',authRoutes)
+
+app.listen(PORT,()=>{
+    console.log('SERVER IS RUNNING ON PORT ',PORT);
+    // console.log("mongodb uri",process.env.MONGODB_URI);
+    
+})
